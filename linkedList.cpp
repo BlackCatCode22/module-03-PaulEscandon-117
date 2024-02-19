2d8ee508467df9a467d6e5628b67f15b544e924e
PAUL ESCANDON <156571752+PaulEscandon-117@users.noreply.github.com>
2024-02-18 19:53:53 -0800

    Add files via upload

---
+++ linkedList.cpp
@@ -0,0 +1,87 @@
+#include <iostream>
+
+struct Node {
+    int data;
+    Node* next;
+
+    Node(int val) : data(val), next(nullptr) {}
+};
+
+class LinkedList {
+private:
+    Node* head;
+
+public:
+    LinkedList() : head(nullptr) {}
+
+    ~LinkedList() {
+        Node* current = head;
+        while (current) {
+            Node* next = current->next;
+            delete current;
+            current = next;
+        }
+    }
+
+    void insertEnd(int val) {
+        Node* newNode = new Node(val);
+        if (!head) {
+            head = newNode;
+            return;
+        }
+        Node* current = head;
+        while (current->next) {
+            current = current->next;
+        }
+        current->next = newNode;
+    }
+
+    void deleteNode(int val) {
+        if (!head)
+            return;
+
+        if (head->data == val) {
+            Node* temp = head;
+            head = head->next;
+            delete temp;
+            return;
+        }
+
+        Node* current = head;
+        while (current->next && current->next->data != val) {
+            current = current->next;
+        }
+
+        if (current->next) {
+            Node* temp = current->next;
+            current->next = current->next->next;
+            delete temp;
+        }
+    }
+
+    void display() {
+        Node* current = head;
+        while (current) {
+            std::cout << current->data << " ";
+            current = current->next;
+        }
+        std::cout << std::endl;
+    }
+};
+
+int main() {
+    LinkedList list;
+    for (int i = 1; i <= 4; ++i) {
+        list.insertEnd(i);
+    }
+
+    std::cout << "Initial list: ";
+    list.display();
+
+    list.deleteNode(3);
+
+    std::cout << "List after deleting node with value 3: ";
+    list.display();
+
+    return 0;
+}
